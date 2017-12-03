99fe1a00307b798a31b5d04536d23c06c761d6c5
wyawen <yw374@cornell.edu>
2017-12-03 03:39:45 -0800

    update scripts for invoking redis client/server lambdas, the script now takes in two cmd line arguments: port_num & log_file_name

--- lambda_invoke_redis_client.sh
+++ lambda_invoke_redis_client.sh
@@ -5,5 +5,5 @@ aws lambda invoke \
 --function-name redisclient \
 --region us-west-1 \
 --log-type Tail \
---payload '{"ip":"13.57.41.133", "port":"8888", "bind_port": "49999"}' \
-outputfile_redis_client.txt &> logs_redis_client.txt
+--payload '{"ip":"54.212.247.168", "port":"8888", "bind_port": "'$1'"}' \
+outputfile_redis_client.txt > $2 2>&1 
--- lambda_invoke_redis_server.sh
+++ lambda_invoke_redis_server.sh
@@ -5,5 +5,5 @@ aws lambda invoke \
 --function-name redisserver \
 --region us-west-2 \
 --log-type Tail \
---payload '{"ip":"54.212.247.168", "port":"8888", "connect_port":"60000"}' \
-outputfile.txt > logs.txt 2>&1
+--payload '{"ip":"54.212.247.168", "port":"8888", "connect_port":"'$1'"}' \
+outputfile.txt > $2 2>&1
