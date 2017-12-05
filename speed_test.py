5b067fea0f7a6b3f4dc6a51c433df1851b70bf5e
Dan Iter <iter.dan@gmail.com>
2017-12-05 11:51:38 -0800

    added s3 tests

--- speed_test.py
+++ speed_test.py
@@ -1,11 +1,13 @@
 from redis import Redis
 import os
 import time
+import boto3
 
+bucket_name = 'd8j32l5-bucket022861'
 
 def do_read_test(size, data, redis):
     times = []
-    runs = 10
+    runs = 100
     key = "TESTKEY"
     redis.set(key, data)
     for i in xrange(runs):
@@ -18,7 +20,7 @@ def do_read_test(size, data, redis):
 
 def do_write_test(size, data, redis):
     times = []
-    runs = 10
+    runs = 100
     key = "TESTKEY"
     for i in xrange(runs):
         start = time.time()
@@ -27,22 +29,50 @@ def do_write_test(size, data, redis):
         times.append(stop - start)
     print "RESULT WRITE: Size", size, "averge time : ", (sum(times) / len(times))
 
+def do_write_s3_test(size, data, s3_client):
+    times = []
+    runs = 100
+    for i in xrange(runs):
+        start = time.time()
+        s3_client.put_object(
+                Bucket = bucket_name,
+                Body = data,
+                Key = 'DANITER_TEST_'+ str(size)
+            )
+        stop = time.time()
+        times.append(stop - start)
+    print "RESULT WRITE: Size", size, "averge time : ", (sum(times) / len(times))
+
+def do_read_s3_test(size, s3_client):
+    times = []
+    runs = 100
+    for i in xrange(runs):
+        start = time.time()
+        s3_client.get_object(
+                Bucket = bucket_name,
+                Key = 'DANITER_TEST_'+ str(size)
+            )['Body'].read()
+        stop = time.time()
+        times.append(stop - start)
+    print "RESULT READ: Size", size, "averge time : ", (sum(times) / len(times))
+
+
 def lambda_handler(event, context):
-    redis = Redis(event['ip'], int(event['port']),
-                  bind_port=int(event['bind_port']))
-    a = redis.set('foo', 'a')
-    foo = redis.get('foo')
-    print "starting", time.time()
-    sizes = [512]#[1024*10, 1024*100 ]#1024*1024, 1024*1024*10, 1024*1024*100]
+    # redis = Redis(event['ip'], int(event['port']),
+    #               bind_port=int(event['bind_port']))
+    # a = redis.set('foo', 'a')
+    # foo = redis.get('foo')
+    # sizes = [1, 1024, 1024*10, 1024*100, 1024*1024 ]#[1024*1024*10, 1024*1024*100] #
+    # data = "a"*(sizes[-1])
+    # for s in sizes:
+    #     do_read_test(s, data[:s], redis)
+    #     do_write_test(s, data[:s], redis)
+    sizes = [1024*10, 1024*100, 1024*1024 ]
     data = "a"*(sizes[-1])
-    print "about to loop", time.time()
-    while True:
-        for s in sizes:
-        #    print "start loop", time.time()
-            do_read_test(s, data[:s], redis)
-        #    print "after read", time.time()
-            do_write_test(s, data[:s], redis)
-        #    print "after write", time.time()
+    s3_client = boto3.client('s3')
+    for s in sizes:
+        do_write_s3_test(s, data[:s], s3_client)
+        do_read_s3_test(s, s3_client)
 
 
 if __name__=='__main__':
